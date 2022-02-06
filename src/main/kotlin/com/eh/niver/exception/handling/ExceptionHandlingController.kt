3a40252cb737c02867c7003779714bc7558b11f9
davidrezende <davidpenalives@gmail.com>
2022-02-05 21:30:19 -0300

    Criando exception handler controller e alteracao para properties yml

---
+++ src/main/kotlin/com/eh/niver/exception/handling/ExceptionHandlingController.kt
@@ -0,0 +1,44 @@
+package com.eh.niver.exception.handling
+
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.WebRequest
+import java.util.*
+import javax.persistence.EntityNotFoundException
+
+@ControllerAdvice(annotations = [RestController::class])
+class ExceptionHandlingController {
+
+    @ExceptionHandler(
+        value = [EntityNotFoundException::class,
+            EmptyResultDataAccessException::class
+        ]
+    )
+    fun handleEntityNotFound(ex: Exception, request: WebRequest): ResponseEntity<ErrorsDetails> {
+        val errorDetails = ErrorsDetails(
+            Date(),
+            "Data not found",
+            ex.message!!
+        )
+        return ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST)
+    }
+
+
+    @ExceptionHandler(value = [(HttpMessageNotReadableException::class)])
+    fun handleHttpMessageNotReadable(ex: HttpMessageNotReadableException, request: WebRequest): ResponseEntity<ErrorsDetails> {
+        val errorDetails = ErrorsDetails(
+            Date(),
+            "Invalid parameters",
+            ex.message!!
+        )
+        return ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST)
+    }
+
+}
+
+data class ErrorsDetails(val time: Date, val message: String, val details: String)
