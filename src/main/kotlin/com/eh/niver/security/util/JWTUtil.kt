064cc2859c2757c805592a7eb167180429aae2db
davidrezende <davidpenalives@gmail.com>
2022-02-16 23:50:40 -0300

    feature: ajustes

--- src/main/kotlin/com/eh/niver/security/util/JWTUtil.kt
+++ src/main/kotlin/com/eh/niver/security/util/JWTUtil.kt
@@ -3,6 +3,7 @@ package com.eh.niver.security.util
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.*
@@ -12,24 +13,34 @@ import java.util.*
 class JWTUtil {
 
     @Value("\${jwt.secret}")
-    private lateinit var secret: String
+    lateinit var secret: String
 
-    private val expiration: Long = 60000
+    @Value("\${jwt.expiration}")
+    lateinit var expiration: String
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(JWTUtil::class.java)
+    }
 
     fun generateToken(username: String): String {
         return Jwts.builder()
             .setSubject(username)
-            .setExpiration(Date(System.currentTimeMillis() + expiration))
+            .setIssuer("http://issuer.com.br")
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + expiration.toLong()))
             .signWith(SignatureAlgorithm.HS512, secret.toByteArray())
             .compact()
     }
 
     fun isTokenValid(token: String): Boolean {
+        logger.info( "token: $token" )
         val claims = getClaimsToken(token)
+        logger.info( "claims: ${claims.toString()}" )
         if (claims != null) {
             val username = claims.subject
             val expirationDate = claims.expiration
             val now = Date(System.currentTimeMillis())
+            logger.info( "data de validada valida: ${now.before(expirationDate)}" )
             if (username != null && expirationDate != null && now.before(expirationDate)) {
                 return true
             }
